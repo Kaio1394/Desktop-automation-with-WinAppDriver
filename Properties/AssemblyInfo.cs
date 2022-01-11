f46a52f4bfe8676ca3df17dddca669bda344a080
kaio <kaio.santiago.13@hotmail.com>
2022-01-11 15:15:09 -0300

    automation with winappdriver

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestOverview")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestOverview")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81c6aa38-a274-4fe4-985a-4ddf968e6021")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestCalculator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+
+namespace MSTestOverview
+{
+    [TestClass]
+    public class TestCalculator
+    {
+        private WindowsDriver<WindowsElement> sessionCalc;
+        private AppiumOptions appOptions;
+
+        [TestInitialize]
+        public void Start()
+        {
+            appOptions = new AppiumOptions();
+            appOptions.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            sessionCalc = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"), appOptions);
+        }
+        [TestCleanup]
+        public void EndTest()
+        {
+            sessionCalc.Quit();
+        }
+
+        [TestMethod]
+        public void TestMethodSum()
+        {
+            
+            sessionCalc.FindElementByAccessibilityId("num3Button").Click();
+            sessionCalc.FindElementByAccessibilityId("plusButton").Click();
+            sessionCalc.FindElementByAccessibilityId("num3Button").Click();
+            sessionCalc.FindElementByAccessibilityId("equalButton").Click();
+        }
+    }
+}
